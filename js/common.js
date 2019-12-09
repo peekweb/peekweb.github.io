e9b37367babe07d061f31d14d0f1560044d57d91
xxx <local@yq>
2019-12-09 12:06:39 +0800

    rand tu

--- js/common.js
+++ js/common.js
@@ -1,9 +1,15 @@
 log = console.log;
 
-function pic(str) {
+function pic(str, rand) {
+    if(rand) {
+        rand = '?' + Math.random();
+    } else {
+        rand = '';
+    }
+
     if(/\dx\d+/.test(str)) {
         var strs = str.split('x');
-        return  'https://img.vpsok.com/w' + strs[0] + 'h' + strs[1] +'.png' + '?' + Math.random();
+        return  'https://img.vpsok.com/w' + strs[0] + 'h' + strs[1] +'.png' + rand ;
     } else {
         return  'https://img.vpsok.com/w450h300.png';
     }
@@ -14,7 +20,11 @@ var $ = document.querySelectorAll.bind(document);
 
 [].forEach.call($('img'), function (elm) {
     if(elm.getAttribute('tu')) {
-        elm.src = pic(elm.getAttribute('tu'));    
+        if(elm.getAttribute('rand')) {
+            elm.src = pic(elm.getAttribute('tu', 'rand'));    
+        } else {
+            elm.src = pic(elm.getAttribute('tu'));    
+        }
     }
 });
 
